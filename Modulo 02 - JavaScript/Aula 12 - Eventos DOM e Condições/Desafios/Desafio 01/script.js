88808d7fc8767adde405d1a6dbd3bf3ed00382b4
NicolasSantana18 <nicolasnss1107fdn@gmail.com>
2025-09-19 10:44:04 -0300

    Aula 12 - Eventos do DOM e Condições

---
+++ Modulo 02 - JavaScript/Aula 12 - Eventos DOM e Condições/Desafios/Desafio 01/script.js
@@ -0,0 +1,9 @@
+function calcularMedia() {
+
+    let nota1 = parseFloat(document.getElementById ("nota1").value)
+    let nota2 = parseFloat(document.getElementById ("nota2").value)
+
+    let media = (nota1 + nota2)/2
+
+    document.getElementById("resultado").innerText = "A resposta é: "+ media.toFixed(2);
+}
