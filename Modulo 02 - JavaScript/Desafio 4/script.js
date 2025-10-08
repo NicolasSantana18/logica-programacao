e6e69a06399d3d4039b3c753e5943bba71d82896
NicolasSantana18 <nicolasnss1107fdn@gmail.com>
2025-10-07 21:22:25 -0300

    .

--- Modulo 02 - JavaScript/Desafio 4/script.js
+++ Modulo 02 - JavaScript/Desafio 4/script.js
@@ -1,16 +1,16 @@
-const alunos = []
+const alunos = [];
 
 function cadastro(){
 
-    let nome = (document.getElementById("nome").value)
-    let n1 = parseInt(document.getElementById("nota").value)
+    let nome = (document.getElementById("nome").value);
+    let nota = parseInt(document.getElementById("nota").value);
 
     let aluno = {
         nome: nome,
-        nota: n1
+        nota: nota
     };
 
-    alunos.push(aluno)
+    alunos.push(aluno);
 
     document.getElementById("lista").innerHTML = alunos
 }
