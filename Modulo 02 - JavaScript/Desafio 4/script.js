8ad5828214b07cc7b5137748a74f2f0e026b0f50
NicolasSantana18 <nicolasnss1107fdn@gmail.com>
2025-10-12 21:40:41 -0300

    .

--- Modulo 02 - JavaScript/Desafio 4/script.js
+++ Modulo 02 - JavaScript/Desafio 4/script.js
@@ -12,6 +12,13 @@ function cadastro(){
 
     alunos.push(aluno);
 
+    if (nome && !isNaN(nota)) {
+        nome.value = ""
+        nota.value = ""
+    } else {
+        alert("Preencha todos os campos")
+    }
+
     
     exibirValores(alunos)
 
@@ -22,7 +29,14 @@ function exibirValores(alunos){
     listaAlunos.innerHTML = ""
     alunos.forEach(aluno => {
         listaAlunos.innerHTML += "<br>" + "<strong>Nome:</strong> " + aluno.nome + " - " + "<strong>Nota:</strong> " + aluno.nota + "<br>"
-    });
+
+        if (aluno.nota >= 7){
+            listaAlunos.style.backgroundColor = "green"
+    }
+        else if (aluno.nota < 7 ) {
+            listaAlunos.style.backgroundColor = "red"
+        }
+        });
 
 }
 
